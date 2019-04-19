cadf3346c8ecbeb76bde7feb3be5464aa2196890
changchiajung <42213793+changchiajung@users.noreply.github.com>
2019-04-19 10:36:47 +0800

    changed model.spy
    更改model架構
    去除存取json 改為使用special_order
    建立mock_data

--- restaurant/noman/models.py
+++ restaurant/noman/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-
+from django.conf import settings
 # Create your models here.
 
 
@@ -10,6 +10,7 @@ class Meal(models.Model):
     detail = models.CharField(max_length=1000,blank=True)
     photo = models.FileField(blank=True)
     money = models.IntegerField(blank=True)
+    category = models.IntegerField(blank= True)
 
     count = models.IntegerField(blank = True)
     def __str__(self):
@@ -19,16 +20,17 @@ class Meal(models.Model):
 class special_meal(models.Model):
     id = models.IntegerField(primary_key=True)
     meal_id = models.ForeignKey(Meal, on_delete=models.CASCADE)
-    count = models.IntegerField(blank = True)
+    number = models.IntegerField(blank = True)
     comment = models.CharField(max_length=500,blank=True)
 
 
 class order(models.Model):
     id = models.IntegerField(primary_key=True)
     meals = models.CharField(max_length=500,blank=True)
+    #store the string of multiple [special meal id]
     progress = models.IntegerField(blank=True)
     other = models.CharField(max_length=1000,blank=True)
-
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def __str__(self):
         return self.id
 
