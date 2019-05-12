f39b06d6cea408d8733655b9efaf422e64e06c24
oucs638 <f74062028@gs.ncku.edu.tw>
2019-05-13 05:11:30 +0800

    add models but not makemigration
    unexpected error before add models
    noman.special_meal has not element "count"

--- restaurant/listManagement/models.py
+++ restaurant/listManagement/models.py
@@ -2,4 +2,32 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
+#class OrderList(models.Model):
+#    user_name = models.CharField(max_length=20)
+#    progress = models.DecimalField(max_digits=3, decimal_places=0)
+#    total_price = models.DecimalField(max_digits=4, decimal_places=0)
+#
+#    def __listProgress__(self):
+#        return self.progress
+#
+#    def __listTotalPrice__(self):
+#        return self.total_price
 
+#class OrderElements(models.Model):
+#    order_name = models.CharField(max_length=20)
+#    order_amount = models.DecimalField(max_digits=3, decimal_places=0)
+#    order_price = models.DecimalField(max_digits=3, decimal_places=0)
+#    order_total_price = models.DecimalField(max_digits=3, decimal_places=0)
+#    order_list = models.ForeignKey(OrderList, on_delete=models.CASCADE)
+#
+#    def __orderName__(self):
+#        return self.order_name
+#
+#    def __orderAmount__(self):
+#        return self.order_amount
+#
+#    def __orderPrice__(self):
+#        return self.order_price
+#
+#    def __orderTotalPrice__(self):
+#        return self.order_total_price
